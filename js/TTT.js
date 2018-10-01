ce2cb33e122165afa8cba82d613510db32c33ddb
Hayden Rainey <s513768@nwmissouri.edu>
2018-09-30 21:23:31 -0500

    game functions as intended.

--- js/TTT.js
+++ js/TTT.js
@@ -1,23 +1,155 @@
 let TTT = function () {
     let huPlayer,
     aiPlayer,
-    origBoard,
+    testBoard,
+    bLoc,
+    bVals,
     fc,
+    turn,
     bestSpot;
 
-    start = function () {
+    //starts game and initializes variables and event listeners 
+    start = function(){
+        bLoc = getBoardLoc();
+        bVals = getBoardVals(); 
+        turn = "P"; 
         huPlayer = "O";
         aiPlayer = "X";
-        //origBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-        origBoard = ["O",1 ,"X","X",4 ,"X", 6 ,"O","O"];
+        initInput();
+    },
+
+    //resets all box values to empty strings 
+    restart = function(){
+        for(let i=0; i < bLoc.length; i++){
+            $(bLoc[i]).html("");
+        }
+        start();
+    },
+    //initialized event listeners 
+    initInput = function(){
+        for(let i=0; i < bLoc.length; i++){
+            $(bLoc[i]).click(function() {
+                if(turn === "P"){
+                    updateBox(i);
+                }
+            });
+        }
+    },
+
+    //turns off event listeners and prevents futher input.  
+    endGame = function(){
+        for(let i =0; i < bLoc.length; i++){
+            $(bLoc[i]).off('click');
+        }
+    }
+
+    //checks turn and allows input if it is currently players turn. 
+    updateBox = function(loc){
+        if(turn === "P"){
+            $(bLoc[loc]).html(huPlayer);
+            bVals = getBoardVals();
+            winCheck();
+            updateTurn();
+        }
+        else if(turn === "A"){
+            $(bLoc[loc]).html(aiPlayer);
+            bVals = getBoardVals();
+            winCheck();//checking for win condition
+            updateTurn();
+        }
+        else{
+            console.log("Incorrect turn ID: " + trn);
+        }
+    },
+
+    //flips turns 
+    updateTurn = function(){
+        switch(turn){
+            case "A": 
+                turn = "P"; 
+                break; 
+            case "P": 
+                turn = "A";
+                aiTurn();
+                break;
+            default:
+                console.log("Incorrect turn ID: " + trn);
+                break;  
+        }
+    },
+
+    aiTurn = function(){
+        setTimeout(updateBox(findBest(bVals)),22000);
+
+    },
+
+    //returns list of box ID's formatted as strings (ex: ["item0","item1"...])
+    getBoardLoc = function(){
+        const b = []; 
+
+        for(let i = 0; i < 9; i++){
+            b.push("#item" + i.toString());
+        }
+
+        return b; 
+    },
+
+    //returns a list of box values (ex: ["0","1","X"])
+    //if no value has been entered ("O" or "X") the index will be entered instead.
+    getBoardVals = function(){
+        const vals = []; 
+
+        for(let i = 0; i< bLoc.length; i++){
+            let l = $(bLoc[i]).html(); 
+            if(l != ""){
+                vals.push(l);
+            }
+            else{
+                vals.push(i);
+            }
+            
+            console.log(vals[i]);
+        }
+
+        return vals; 
+    },
+
+    //checks for winning conditions 
+    winCheck = function(){
+        if(winning(bVals,huPlayer)){
+            console.log("i should be writting a message");
+            $("#winBanner").html("You won!")
+            endGame();
+        }
+        else if(winning(bVals,aiPlayer)){
+            console.log("i should be writting a message");
+            $("#winBanner").html("You lose!");
+            endGame();
+        }
+        else if(emptyIndexies(bVals).length <= 0){
+            $("#winBanner").html("No one wins!");
+            endGame();
+        }
+    },
+
+    //returns the best move based on the current board state
+    findBest = function (board) {
+        
+        //testBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        //testBoard = ["O",1 ,"X","X",4 ,"X", 6 ,"O","O"];
+        console.log(board);
         fc = 0;
-        bestSpot = minimax(origBoard, aiPlayer);
+        bestSpot = minimax(board, aiPlayer);
 
         console.log("index: " + bestSpot.index);
         console.log("function calls: " + fc);
 
+        return bestSpot.index;
     },
 
+    //minimax function curtesy of Ahmad Abdolsaheb of FreeCodeCamp.org
+    //https://medium.freecodecamp.org/how-to-make-your-tic-tac-toe-game-unbeatable-by-using-the-minimax-algorithm-9d690bad4b37
+    //recursive function that checks each potential move and returns a score. Moves with the highest score are returned. 
     minimax = function (newBoard, player) {
         fc++
 
@@ -102,8 +234,6 @@ let TTT = function () {
 
     return{
         start: start,
-        minimax: minimax,
-        winning: winning,
-        emptyIndexies, emptyIndexies
+        restart: restart
     };
 } ();
